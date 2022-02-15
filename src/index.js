c017bb9f4bc19c6bacced9bd39880ed0b475eec0
Tapi33 <artem8554134@gmail.com>
2022-02-15 21:00:41 +0300

    feat:complete the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    if (n > 0){
+        return n.toString().split('').reverse().join('');
+    } else {
+        let arr = n.toString().split('');
+        delete arr[0];
+        return arr.reverse().join('');
+    }
 }
